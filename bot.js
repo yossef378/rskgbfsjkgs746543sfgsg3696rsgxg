0ebf24781fbebb55f0c74184c6a26a0ecb2d62df
yossef378 <45338265+yossef378@users.noreply.github.com>
2018-11-26 12:13:34 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,7 +14,7 @@ client.user.setStatus("dnd")
 
 client.on("message", async message => {
         if(!message.channel.guild) return;
- var prefix= "+";
+ var prefix= "$";
         if(message.content.startsWith(prefix + 'server')) {
         let guild = message.guild
         let channel = message.channel
