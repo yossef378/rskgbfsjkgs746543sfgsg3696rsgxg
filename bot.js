9b08034f0f7315b28256460aa7b5898ac76bc189
yossef378 <45338265+yossef378@users.noreply.github.com>
2018-11-27 10:24:05 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,21 @@ client.user.setGame(`Hi im pearl gaming bot`,"www.pearlgamingfourm.ga")
 client.user.setStatus("dnd")
 });
 
-
+client.on('message', async msg => {
+const devs = ['Your ID here'];
+if(!devs.includes(message.author.id)) return;
+  if(msg.author.bot) return;
+  let prefix = "$?";
+  if(!msg.content.startsWith(prefix)) return;
+  let command = msg.content.split(" ")[0].slice(prefix.length);
+  let args = msg.content.split(" ").slice(1);
+ 
+  if(command == 'say') {
+    if(!args.join(" ")) return msg.delete();
+    msg.channel.send(args.join(" "));
+    return;
+  }
+});
 
 client.on('message', message => {
  var prefix = "$"
